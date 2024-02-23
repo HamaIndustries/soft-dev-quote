5b82f8b684e8b4b32089c90a8ba6f893634b6fa8
FrankENG17 <olivarezfrank98@gmail.com>
2024-02-23 14:44:51 -0600

    Added Login and Quote History
    need to add cleanup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,6 +21,12 @@ const router = createRouter({
     },
 
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+
+    {
       path: '/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue')
@@ -30,6 +36,12 @@ const router = createRouter({
       path: '/profile',
       name: 'profile',
       component: () => import('../views/profileView.vue')
+    },
+
+    {
+      path: '/quotehistory',
+      name: 'quotehistory',
+      component: () => import('../views/QuoteHistory.vue')
     }
   ]
 })
