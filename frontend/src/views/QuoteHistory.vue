ef08c1011a2b4ef73b153193b955cc64be569cc2
FrankENG17 <olivarezfrank98@gmail.com>
2024-02-23 16:32:40 -0600

    Completed Login and Quote History

--- frontend/src/views/QuoteHistory.vue
+++ frontend/src/views/QuoteHistory.vue
@@ -1,70 +1,76 @@
 <template>
-    <h1>Quote History</h1>
+    <h1 class="QuoteHeader">Quote History</h1>
     <div 
         class="table-row"
         v-for="row in quotes"
+        :key="row.client"
     >
         <div
             class="table-column_item"
             v-for="column in Object.keys(row)"
+            :key="column"
         >
-            {{  row[column] }}
+            {{ row[column] }}
         </div>
     </div>
 </template>
 
-
 <script setup>
-
-const quotes = [
-    {
-      client: 'company1',
-      gallonsRequested: 1000,
-      deliveryAddress: 'texas',
-      deliveryDate: 'December',
-      suggestedPriceperGallon: 50,
-      totalAmountdue: 5000
-    },
-    
-    {
-      client: 'company2',
-      gallonsRequested: 1000,
-      deliveryAddress: 'texas',
-      deliveryDate: 'December',
-      suggestedPriceperGallon: 50,
-      totalAmountdue: 5000
-    },
-    
-    {
-      client: 'company3',
-      gallonsRequested: 1000,
-      deliveryAddress: 'texas',
-      deliveryDate: 'December',
-      suggestedPriceperGallon: 50,
-      totalAmountdue: 5000
-    }
-  ]
-
+import { defineProps } from 'vue';
 
 const props = defineProps({
     quotes: {
-        type: Object,
+        type: Array,
         required: false
     }
-})
+});
 
-props.quotes = quotes
-
+const quotes = [
+    {
+        client: 'company1',
+        gallonsRequested: 1000,
+        deliveryAddress: 'Texas',
+        deliveryDate: 'December',
+        suggestedPriceperGallon: 50,
+        totalAmountdue: 5000
+    },
+    {
+        client: 'company2',
+        gallonsRequested: 1500,
+        deliveryAddress: 'California',
+        deliveryDate: 'January',
+        suggestedPriceperGallon: 50,
+        totalAmountdue: 7500
+    },
+    {
+        client: 'company3',
+        gallonsRequested: 2000,
+        deliveryAddress: 'New York',
+        deliveryDate: 'February',
+        suggestedPriceperGallon: 50,
+        totalAmountdue: 10000
+    }
+];
 </script>
 
 <style scoped>
+.QuoteHeader {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+    margin-top: 60px;
+    margin-bottom: 10px;
+}
 .table-row {
     display: flex;
     flex-wrap: nowrap;
+    background: white;
 }
-
-.table-colum_item {
-    width: 10%;
+.table-column_item {
+    color: black;
+    width: 20%;
     border: solid 1px #4d4d4d;
     padding: 8px 16px;
     box-sizing: border-box;
