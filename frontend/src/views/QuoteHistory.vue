d70f418e84857f01e42074694149afc15567bce8
eliatlarge <eliatlarge@gmail.com>
2024-02-23 17:40:36 -0600

    clean up quote history

--- frontend/src/views/QuoteHistory.vue
+++ frontend/src/views/QuoteHistory.vue
@@ -1,22 +1,32 @@
 <template>
     <h1 class="QuoteHeader">Quote History</h1>
-    <div 
-        class="table-row"
-        v-for="row in quotes"
-        :key="row.client"
-    >
-        <div
-            class="table-column_item"
-            v-for="column in Object.keys(row)"
-            :key="column"
-        >
-            {{ row[column] }}
-        </div>
-    </div>
+    <table>
+        <thead>
+            <tr class="table-row">
+                <th class = "table-column_item table-header_item"
+                    v-for = "col in cols"
+                    :key = "col"
+                >{{ labels[col] }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class = "table-row"
+                v-for = "row in quotes"
+                :key = "row.client"
+            >
+                <td
+                    class="table-column_item"
+                    v-for="column in cols"
+                    :key="column"
+                >
+                    {{ row[column] }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
 
 const props = defineProps({
     quotes: {
@@ -24,6 +34,10 @@ const props = defineProps({
         required: false
     }
 });
+
+const cols = [
+    "client", "gallonsRequested", "deliveryAddress", "deliveryDate", "suggestedPriceperGallon", "totalAmountdue"
+]
 
 const quotes = [
     {
@@ -51,6 +65,15 @@ const quotes = [
         totalAmountdue: 10000
     }
 ];
+
+const labels = {
+    client: 'Client',
+    gallonsRequested: "Gallons",
+    deliveryAddress: 'Delivery State',
+    deliveryDate: 'Delivery Date',
+    suggestedPriceperGallon: "Suggested Price",
+    totalAmountdue: "Total Quoted"
+}
 </script>
 
 <style scoped>
@@ -64,15 +87,16 @@ const quotes = [
     margin-bottom: 10px;
 }
 .table-row {
-    display: flex;
-    flex-wrap: nowrap;
     background: white;
 }
 .table-column_item {
     color: black;
-    width: 20%;
     border: solid 1px #4d4d4d;
     padding: 8px 16px;
     box-sizing: border-box;
+}
+
+.table-header_item {
+    background-color: lightgray;
 }
 </style>
