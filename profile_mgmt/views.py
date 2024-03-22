4026c5f4f0e45fff7266c275d026741bebd85f76
Quang Le <106447805+Gyurgious@users.noreply.github.com>
2024-03-21 20:11:10 -0500

    Profile_mgmt module
    - connected profile management backend to frontend (sends dummy data)
    - created unit tests for my view
    - setted up validation models

--- profile_mgmt/views.py
+++ profile_mgmt/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+import datetime
+
+"""
+def current_datetime(request):
+    now = datetime.datetime.now()
+    data = {'current_datetime': now}
+    return JsonResponse(data)
+"""
+
+
+def profile_mgmt_api(request):
+    
+    if request.method == 'GET':  # handle GET requests
+        print("Fetching data from the backend")  
+      
+        data = {
+            'name': 'Geto Suguru',
+            'address1': '1234 Tokyo Str',
+            'address2': '',
+            'city': 'Tokyo',
+            'state': 'JP',
+            'zipcode': '12345',
+        }
+        return JsonResponse(data)
+    
+    elif request.method == 'POST':  # Handle POST requests
+        data = {'message': 'Data received successfully'}
+        return JsonResponse(data)
+    
+
